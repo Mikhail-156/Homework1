f86b1335b3b920d2146eb38cbbb77a6e942dbf70
Михаил Ергунов <ergunov.mih@mail.ru>
2025-03-27 17:13:25 +0500

    homework 10.2 test_masks

--- src/masks.py
+++ src/masks.py
@@ -1,15 +1,24 @@
 from typing import Union
 
-card_number = input()
-account_number = input()
-"""Получение даных пользователя"""
-
 
 def get_mask_card_number(card_number: Union[int, str]) -> Union[str]:
     """Функция которя скрывает номет катры"""
-    return f"{str(card_number)[:4]}, {str(card_number)[4:7]}**, ****, {str(card_number)[-4:]}"
+    if len(str(card_number)) == 0:
+        return "Введите номер карты"
+    elif len(str(card_number)) != 16:
+        return "Номер должин состоять из 16 цифр"
+    elif card_number.isdigit():
+        return f"{str(card_number)[:4]} {str(card_number)[4:6]}** **** {str(card_number)[-4:]}"
+    return "Номер должин содержать только цифры"
 
 
 def get_mask_account(account_number: Union[int, str]) -> Union[str]:
     """Функция которя скрывает номет счета"""
-    return f"**{str(account_number)[-4:]}"
+    if len(str(account_number)) == 0:
+        return "Введите номер счета"
+    elif len(str(account_number)) != 20:
+        return "Номер должин состоять из 20 цифр"
+    elif account_number.isdigit():
+        return f"**{str(account_number)[-4:]}"
+    return "Номер должин содержать только цифры"
+
