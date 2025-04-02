ff0109472502c9ee55cf5aca4aaa087fc4db9306
Михаил Ергунов <ergunov.mih@mail.ru>
2025-04-02 08:57:08 +0500

    homework 10.3 new_module

--- src/masks.py
+++ src/masks.py
@@ -7,7 +7,7 @@ def get_mask_card_number(card_number: Union[int, str]) -> Union[str]:
         return "Введите номер карты"
     elif len(str(card_number)) != 16:
         return "Номер должин состоять из 16 цифр"
-    elif card_number.isdigit():
+    elif str(card_number).isdigit():
         return f"{str(card_number)[:4]} {str(card_number)[4:6]}** **** {str(card_number)[-4:]}"
     return "Номер должин содержать только цифры"
 
@@ -18,6 +18,6 @@ def get_mask_account(account_number: Union[int, str]) -> Union[str]:
         return "Введите номер Счета"
     elif len(str(account_number)) != 20:
         return "Номер должин состоять из 20 цифр"
-    elif account_number.isdigit():
+    elif str(account_number).isdigit():
         return f"**{str(account_number)[-4:]}"
     return "Номер должин содержать только цифры"
