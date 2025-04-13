b2e272d98f0cf0da67fd8e5d3d9719ca65a7e7e0
Михаил Ергунов <ergunov.mih@mail.ru>
2025-04-13 08:03:39 +0500

    homework 11.2 decorators

---
+++ src/decorators.py
@@ -0,0 +1,30 @@
+import time
+
+def log(filename=None):
+    def my_decorator(func):
+        def inner(*args, **kwargs):
+
+            try:
+                print(args)
+                result = func(*args, **kwargs)
+                message = f'{func.__name__} OK {time.asctime()}\n'
+            except Exception as error:
+                result = None
+                message = f'{func.__name__} {time.asctime()} error: {error}. Inputs: {args}, {kwargs}\n'
+            finally:
+                if filename:
+                    with open(filename, "a", encoding='utf-8') as log_file:
+                        log_file.write(message)
+                else:
+                    print(message)
+
+            return result
+        return inner
+    return my_decorator
+
+
+@log("log.txt")
+def _summ(a, b):
+    print(a + b)
+
+_summ(5, 12)
