5a968333ca19dc7127c58c61cc7dc6bccde01426
Михаил Ергунов <ergunov.mih@mail.ru>
2025-04-20 14:24:04 +0500

    homework 12.1 changes "README"

--- src/external_api.py
+++ src/external_api.py
@@ -10,17 +10,13 @@ def convert_to_rub(transaction: dict) -> float:
     """Конвертирует сумму транзакции в рубли."""
     amount = transaction['amount']
     currency = transaction['currency']
-
     if currency == 'RUB':
         return float(amount)
-
     url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
     headers = {
         "apikey": os.getenv("API_KEY")
     }
-
     response = requests.get(url, headers=headers)
-
     if response.status_code == 200:
         return float(response.json()['result'])
     else:
